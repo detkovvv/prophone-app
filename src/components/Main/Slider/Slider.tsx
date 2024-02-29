04fb36be81f33ccce386a77983382fcbc6039b6f
detkov <fgh.detkov@gmail.com>
2024-02-29 20:26:20 +0300

    refactoring Slider

--- src/components/Main/Slider/Slider.tsx
+++ src/components/Main/Slider/Slider.tsx
@@ -15,10 +15,16 @@ export const Slider: FC<{ sliderItems: PhonesType }> = ({ sliderItems }) => {
                         <h1 className={style.title}> {slide.name}</h1>
                         <p className={style.description}>{slide.descriptions}</p>
                         <div className={style.prices}>
-                            <div className={style.new_price} style={{ color: `${slide.fill}` }}>
+                            <div
+                                className={style.new_price}
+                                style={{ color: `${slide.backgroundColor}` }}
+                            >
                                 {slide.price.newPrice}
                             </div>
-                            <div className={style.old_price} style={{ color: `${slide.fill}` }}>
+                            <div
+                                className={style.old_price}
+                                style={{ color: `${slide.backgroundColor}` }}
+                            >
                                 {slide.price.oldPrice}
                             </div>
                         </div>
@@ -28,23 +34,24 @@ export const Slider: FC<{ sliderItems: PhonesType }> = ({ sliderItems }) => {
                             alt={slide.image}
                             loading='lazy'
                         />
+                        <svg
+                            className={style.big_logo}
+                            width='418.89'
+                            height='538.61'
+                            viewBox='0 0 418.89 538.61'
+                            fill='none'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                d='M243.632 538.609H68.3701L243.632 273.579H418.895L243.632 538.609Z'
+                                fill={slide.fill}
+                            />
+                            <path
+                                d='M179.537 0H359.074L179.537 273.58H0L179.537 0Z'
+                                fill={slide.fill}
+                            />
+                        </svg>
                     </div>
-                    <svg
-                        width='377'
-                        height='517'
-                        viewBox='0 0 377 517'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
-                        <path
-                            d='M243.632 538.609H68.3701L243.632 273.579H418.895L243.632 538.609Z'
-                            fill={slide.fill}
-                        />
-                        <path
-                            d='M179.537 0H359.074L179.537 273.58H0L179.537 0Z'
-                            fill={slide.fill}
-                        />
-                    </svg>
                 </div>
             ))}
         </div>
